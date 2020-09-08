b44e06c14811e4a2ce1f21a703ebc9ab49a69bd1
Hylsebeck <58297386+Hylsebeck@users.noreply.github.com>
2020-09-08 10:07:13 +0200

    Nu med luk og åben funktion

--- UR.py
+++ UR.py
@@ -1,5 +1,9 @@
 from ur_programmer import UR_programmer
 from rTData import RTData
+import tkinter as tk
+from math import sin
+import time
+import socket
 
 prog = UR_programmer("10.130.58.14", simulate=False)
 
@@ -18,10 +22,10 @@ print('Lilla        - Kør til lilla')
 print('Pink         - Kør til pink')
 
 #Punkter:
-Red_move = b'    movej(p[-0.46694534589480835, -0.37128454680967377, 0.04719900890326209, -2.2179237839798986, -2.22213649039662, -0.0015964512197490473])\n'
-Blue_move = b'    movej(p[-0.3693862960510677, -0.46808488913304924, 0.04724995447689745, -2.2179385532480334, -2.2220897902105032, -0.0014254072396997683])\n'
-Pink_move = b'    movej(p[-0.3693534253982512, -0.37132993152269583, 0.047187788409537745, -2.2179978175123582, -2.222064216064882, -0.0016359576113810165])\n'
-Purple_move = b'    movej(p[-0.46692173996805797, -0.468102314883946, 0.047183831068495635, -2.2179022788830376, -2.2221380390946655, -0.0016503394839434411])\n'
+prog.Red_move = b'    movej(p[-0.46694534589480835, -0.37128454680967377, 0.04719900890326209, -2.2179237839798986, -2.22213649039662, -0.0015964512197490473])\n'
+prog.Blue_move = b'    movej(p[-0.3693862960510677, -0.46808488913304924, 0.04724995447689745, -2.2179385532480334, -2.2220897902105032, -0.0014254072396997683])\n'
+prog.Pink_move = b'    movej(p[-0.3693534253982512, -0.37132993152269583, 0.047187788409537745, -2.2179978175123582, -2.222064216064882, -0.0016359576113810165])\n'
+prog.Purple_move = b'    movej(p[-0.46692173996805797, -0.468102314883946, 0.047183831068495635, -2.2179022788830376, -2.2221380390946655, -0.0016503394839434411])\n'
 
 while not inp.startswith('q'):
     inp = input('> ')
@@ -83,4 +87,44 @@ while not inp.startswith('q'):
     elif inp == "Pink":
         prog.s.send(b'def myProg():\n')
         prog.s.send(prog.Pink_move)
-        prog.s.send(b'end\n')    
+        prog.s.send(b'end\n')
+
+    elif inp =="Luk":
+        TCP_PORT = 29999
+        BUFFER_SIZE = 1024
+        TCP_IP = "10.130.58.14"#INDTAST DEN RIGTIGE IP-ADRESSE HER!
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10)
+        try:
+            s.connect((TCP_IP, TCP_PORT))
+            response = s.recv(BUFFER_SIZE)
+        except socket.error:
+            print("Socket error")
+            s.close()
+
+        st = "load /programs/henrik3d/luk.urp\n"
+        s.send(bytearray(st,'utf8'))
+        response = s.recv(BUFFER_SIZE)
+        s.send(b"play\n")
+        response = s.recv(BUFFER_SIZE)
+        s.close()
+
+    elif inp =="Åben":
+        TCP_PORT = 29999
+        BUFFER_SIZE = 1024
+        TCP_IP = "10.130.58.14"#INDTAST DEN RIGTIGE IP-ADRESSE HER!
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10)
+        try:
+            s.connect((TCP_IP, TCP_PORT))
+            response = s.recv(BUFFER_SIZE)
+        except socket.error:
+            print("Socket error")
+            s.close()
+
+        st = "load /programs/henrik3d/open.urp\n"
+        s.send(bytearray(st,'utf8'))
+        response = s.recv(BUFFER_SIZE)
+        s.send(b"play\n")
+        response = s.recv(BUFFER_SIZE)
+        s.close()
