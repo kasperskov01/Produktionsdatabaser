94b3842e771980fdd62550b706bb75fac8b3ed00
tobi8380 <54858460+tobi8380@users.noreply.github.com>
2020-09-08 08:23:12 +0200

    fjerner models og service

--- service.py
+++
@@ -1,8 +0,0 @@
-from models import UserModel
-
-class UserService:
-    def __init__(self):
-        self.model = UserModel()
-    
-    def create(self, params):
-        return self.model.create(params)
