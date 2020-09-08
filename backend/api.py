80c7c3797b3282c8102a72ed578bca3703fec020
tobi8380 <tobi8380@gmail.com>
2020-09-08 17:16:29 +0200

    to_return

--- backend/api.py
+++ backend/api.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# cors = CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 db = SQLAlchemy(app)
@@ -25,15 +24,8 @@ class users(db.Model):
     def __repr__(self):
         return "<bruger_id %r" % self.id
 
-# @app.route('/api/', methods=["POST"])
-# def main_interface():
-#     response = request.get_json()
-#     ny_bruger = users(username=response["message"])
-#     print(ny_bruger.username)
-
 
 @app.route('/api/user/login', methods=["POST"])
-# @cross_origin()
 def login():
     response = request.get_json()
     username = response["username"]
@@ -51,14 +43,6 @@ def login():
     print(to_return)
     return jsonify(to_return)
 
-# @app.after_request
-# def after_request(response):
-#   response.headers.add('Access-Control-Allow-Origin', 'http://127.0.0.1:5500')
-#   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-#   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-#   response.headers.add('Access-Control-Allow-Credentials', 'true')
-#   return response
-    
 
 @app.after_request
 def add_headers(response):
