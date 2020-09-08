2d3fb68fef8c0478790b16024151358ed78494ba
tobi8380 <tobi8380@gmail.com>
2020-09-08 17:55:20 +0200

    login og opret

--- backend/api.py
+++ backend/api.py
@@ -12,35 +12,60 @@ def database_init():
 
 class users(db.Model):
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(30), nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    _id = db.Column("id", db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    user_type = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, user_type):
         self.username = username
         self.password = password
-
+        self.user_type = user_type
 
     def __repr__(self):
-        return "<bruger_id %r" % self.id
+        return "<bruger_id %r" % self._id
 
+
+@app.route('/api/user/signup', methods=["POST"])
+def opret():
+    response = request.get_json()
+    username = response["username"]
+    password = response["password"]
+    user_type = response["user_type"]
+
+    found_user = users.query.filter_by(username=username).first()
+
+    if found_user:
+        to_return = {"user_exists": True}
+        print("user already created")
+    else:
+        new_user = users(username, password, user_type)
+        db.session.add(new_user)
+        db.session.commit()
+        to_return = {"user_created": True}
+        print("user created")
+    
+    return jsonify(to_return)
 
 @app.route('/api/user/login', methods=["POST"])
 def login():
     response = request.get_json()
     username = response["username"]
     password = response["password"]
-
-    # found_user = users.query.filter_by(username=username).first()
-    # if found_user:
-    #     pass
-    # else:
-    #     new_user = users(response["username"],response["password"])
-    #     db.session.add(new_user)
-    #     db.session.commit()
     
-    to_return = {"username": response["username"], "logged_in": True, "user_type": "kunde"}
-    print(to_return)
+
+    found_user = users.query.filter_by(username=username).first()
+    if found_user:
+        if password == found_user.password:
+            to_return = {"username": username, "logged_in": True, "user_type": found_user.user_type}
+            print("logged in")
+        else:
+            to_return = {"username": username, "logged_in": False, "user_type": found_user.user_type}
+            print("password incorrect")
+    else:
+        print("user not found")
+        to_return = {"logged_in": False}
+    
     return jsonify(to_return)
 
 
@@ -51,4 +76,6 @@ def add_headers(response):
     return response
 
 if __name__ == '__main__':
+    db.drop_all()
+    db.create_all()
     app.run(debug=True)
