1763c9fdba8ffb318f93702e2b41602c15148cf6
Hylsebeck <58297386+Hylsebeck@users.noreply.github.com>
2020-09-17 11:20:53 +0200

    Brr

---
+++ robot/rest_api.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def robot_order_get():
+    #query = {'username':'Kasper', 'password':'123', 'user_type':'chef'}
+    response = requests.get('https://proddb.herokuapp.com/api/robot/order/get')
+    try:
+        data = response.json()
+        print(data)
+        print(data["order"])
+        return data
+    except:
+        print("Fejl opstod")
+
+def robot_order_status_set(order_id, status):
+    query = {'order_id':order_id, 'status':status}
+    response = requests.post('https://proddb.herokuapp.com/api/robot/order/status/set', json=query)
+    try:
+        data = response.json()
+        print(data)
+        print(data["update_status"])
+        return data
+    except:
+        print("Fejl opstod")
+    
